466ee06c7d4f59772c37adceb66a154ea23c3fa1
Bruno <bvaleiro@gmail.com>
2022-05-09 20:01:47 -0300

    fisrt commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-
-import GlobalStyles from 'styles/global'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '../styles/global'
+import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>WonGame</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
@@ -19,7 +20,7 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
